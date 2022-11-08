b120287496968638501d2053d9ad6984b4733a38
thyttan <97237430+thyttan@users.noreply.github.com>
2022-11-08 20:17:26 +0100

    Update app.js

--- apps/books/app.js
+++ apps/books/app.js
@@ -2,7 +2,9 @@
 Todo
 - [DONE] remove last entry in words array (It's often a incomplete word b/c of how book.txt is read with require)
   - [Done] read the size in bytes of the string removed and consider it when reading the next section of book.txt
--implement scrolling back and forth seemlessly between sections of book.txt
+-implement scrolling back and forth seemlessly between sections of book.txt. [started]
+- is it better to store the book as a json array than a .txt file? What does that mean for file size?
+- should I have a count of number of words handy in a help file? I think this would help with scrolling back and forth in the text.
 */
 
 
@@ -28,7 +30,10 @@ function setUI() { Bangle.setUI({
   drag : function(e) {
     counter = counter+e.dx/20 < 0 ? 0: Math.round(counter+e.dx/20); // Update counter to move back and forth in the text.
     // Todo: If counter <= 0 updateWords to the last section of book.txt, and vice versa if scrolling beyond the words currently stored in 'words'.
-    
+    if (counter == 0) {
+      words = updateWords(accumulatedLength - targetLength);
+      counter = words.length-1 + Math.round(counter+e.dx/20);
+    }
     //counter = counter > words.length ? words.length : counter;
     
     wordsPerSecond = wordsPerSecond-e.dy/40; // Change reading speed.
@@ -66,19 +71,35 @@ function setUI() { Bangle.setUI({
 });
 }
 
-targetLength = 20*2; // in bytes: (# of characters)*(bytes per character) https://stackoverflow.com/a/46735247
+targetLength = 40; // in bytes, 1 byte per character ???
 let actualLength;
 let accumulatedLength = 0;
+let accumulatedWords = 0;
 section = 0;
 function updateWords(startPoint) {
-  sentences = require("Storage").read("book.txt", startPoint, targetLength);
+  if (startPoint >= 0) {
+    sentences = require("Storage").read("book.txt", startPoint, targetLength);
+  } else {
+    sentences = require("Storage").read("book.txt", 0, targetLength+startPoint);
+  }
   words = sentences.split(" ");
-  // remove the last word from words, it's probably incomplete.
-  poped = words.pop();
-  // remove that size in bytes from length variable
-  bytesOfPoped = poped.length*2; // https://stackoverflow.com/a/46735247
-  actualLength = targetLength - bytesOfPoped;
-  accumulatedLength += actualLength;
+  print(words);
+  // remove the last/first word from words, it's probably incomplete.
+  if (startPoint >= accumulatedLength) {
+    removed = words.pop();
+    bytesOfRemoved = removed.length; //*2; // https://stackoverflow.com/a/46735247 ????
+    // remove that size in bytes from length variable
+    actualLength = targetLength - (bytesOfRemoved);
+    accumulatedLength += actualLength;
+  } else if (startPoint < accumulatedLength) {
+    removed = words.shift();
+    bytesOfRemoved = removed.length; //*2; // https://stackoverflow.com/a/46735247 ????
+    // remove that size in bytes from length variable
+    actualLength = targetLength - (bytesOfRemoved);
+    accumulatedLength -= actualLength;
+    accumulatedWords += words.length;
+  }
+  //print(words);
   return words;
 }
 
@@ -138,6 +159,6 @@ function startUp() {
 }
 
 // Entry point: First use keyboard to ask where to start reading, then update setUI and start reading.
-require("textinput").input({text:"enter word#: "}).then(result => {counter = !parseInt(result.slice(13))?0:parseInt(result.slice(13));}).then(setUI).then(startUp);
-//setUI();
-//startUp();
+//require("textinput").input({text:"enter word#: "}).then(result => {counter = !parseInt(result.slice(13))?0:parseInt(result.slice(13));}).then(setUI).then(startUp);
+setUI();
+startUp();
