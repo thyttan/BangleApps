8f1d139d6ef7bc60cbf9dd579932f1e18da639cd
thyttan <6uuxstm66@mozmail.com>
2023-08-21 23:54:50 +0200

    slidertest: refactor to not use LCD overlay

--- apps/slidertest/app.js
+++ apps/slidertest/app.js
@@ -2,10 +2,13 @@
 let callback = (mode,fb)=>{
   if (mode =="map") Bangle.musicControl({cmd:"volumesetlevel",extra:Math.round(100*fb/30)});
   if (mode =="incr") Bangle.musicControl(fb>0?"volumedown":"volumeup");
-  if (mode =="remove") {audioLevels.c = fb; ebLast = 0;}
+  if (mode =="remove") {audioLevels.c = fb; ebLast = 0; draw();}
 };
 
-g.reset().clear().setColor(1,0,0).fillRect(0,0,176,176);
+let draw = ()=>{
+  g.reset().clear().setColor(1,0,0).fillRect(0,0,176,176);
+};
+draw();
 
 let audioLevels = {u:30, c:15}; // Init with values to avoid "Uncaught Error: Cannot read property 'u' of undefined" if values were not gathered from Gadgetbridge.
 let audioHandler = (e)=>{audioLevels = e;};
@@ -16,7 +19,7 @@ let ebLast = 0; // Used for fix/Hack needed because there is a timeout before th
 Bangle.on('drag', (e)=>{
   if (ebLast==0) {
   Bangle.musicControl("volumegetlevel");
-  setTimeout(()=>{require("SliderInput").interface(callback, {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c, horizontal:true});},200);
+  setTimeout(()=>{require("SliderInput").interface(callback, {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c, horizontal:false});},200);
   }
   ebLast = e.b;
 }
