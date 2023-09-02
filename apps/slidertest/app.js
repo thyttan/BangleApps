3ba7c90ff886aef0d5bdd035b03e53ecffdf6c31
thyttan <6uuxstm66@mozmail.com>
2023-09-02 16:13:52 +0200

    slidertest: propagate drags through both sliders
    And some reformatting.

--- apps/slidertest/app.js
+++ apps/slidertest/app.js
@@ -3,7 +3,9 @@ let callback = (mode,fb)=>{
   if (mode =="map") Bangle.musicControl({cmd:"volumesetlevel",extra:Math.round(100*fb/30)});
   if (mode =="incr") Bangle.musicControl(fb>0?"volumedown":"volumeup");
   if (mode =="remove") {
-    audioLevels.c = fb; ebLast = 0; draw();
+    audioLevels.c = fb;
+    ebLast = 0;
+    draw();
     sliderObject2.f.draw(sliderObject2.v.level);
     print(process.memory().usage);
     print("#drag handlers: " + Bangle["#ondrag"].length)
@@ -27,7 +29,10 @@ let draw = ()=>{
 let sliderObject2;
 let init = ()=> {
   draw();
-  sliderObject2 = require("SliderInput").interface(callback2, {useMap:true, steps:30, currLevel:currentLevel, horizontal:true, rounded:false, timeout:0, useIncr:false, immediateDraw:false, propagateDrag:true, width:Math.round(Bangle.appRect.w/20), xStart:R.x2-R.w/20-4, oversizeR:10, oversizeL:10, autoProgress:true});
+  sliderObject2 = require("SliderInput").interface(
+      callback2,
+      {useMap:true, steps:30, currLevel:currentLevel, horizontal:true, rounded:false, timeout:0, useIncr:false, immediateDraw:false, propagateDrag:true, width:Math.round(Bangle.appRect.w/20), xStart:R.x2-R.w/20-4, oversizeR:10, oversizeL:10, autoProgress:true}
+    );
   sliderObject2.f.draw(sliderObject2.v.level);
   sliderObject2.f.startAutoUpdate();
 }
@@ -41,14 +46,14 @@ init();
 
 let ebLast = 0; // Used for fix/Hack needed because there is a timeout before the slider is called upon.
 let sliderObject=require("SliderInput").interface(
-    callback, 
-    {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c, horizontal:false, rounded:false, height: R.h-21, timeout:0.5}
+    callback,
+    {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c, horizontal:false, rounded:false, height: R.h-21, timeout:0.5, propagateDrag:true}
   );
 
 Bangle.on('drag', (e)=>{
   if (ebLast==0) {
     Bangle.musicControl("volumegetlevel");
-    if (e.y<140) {
+    if (e.y<140 && !sliderObject.v.dragActive) {
       setTimeout(()=>{
         sliderObject.c.steps=audioLevels.u;
         sliderObject.v.level=audioLevels.c;
@@ -56,7 +61,7 @@ Bangle.on('drag', (e)=>{
       sliderObject.v.dy = 0;
       Bangle.prependListener('drag', sliderObject.f.dragSlider);
     }
-    if (e.y>=140) {
+    if (e.y>=140 && !sliderObject2.v.dragActive) {
       Bangle.prependListener('drag',sliderObject2.f.dragSlider);
     }
   }
