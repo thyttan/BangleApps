5362306b45eae38e5e6bcc6715f6ae21e813f0ac
thyttan <6uuxstm66@mozmail.com>
2023-09-09 16:10:33 +0200

    slidertest: reorder code to fix reference error

--- apps/slidertest/app.js
+++ apps/slidertest/app.js
@@ -21,6 +21,33 @@ setTimeout(()=>{g.reset().setColor(1,0,0).fillRect(R.x2/2-5,R.y2/2-5,R.x2/2+5,R.
 
 //// Functional logic
 
+// Get audio levels from Android device
+let audioLevels = {u:30, c:15}; // Init with values to avoid "Uncaught Error: Cannot read property 'u' of undefined" if values were not gathered from Gadgetbridge.
+let audioHandler = (e)=>{audioLevels = e;print(audioLevels);};
+Bangle.on('audio', audioHandler);
+Bangle.musicControl("vg"); // vg = Volume Get level
+
+// Handle music messages
+// Bangle.emit("message", type, msg);
+let trackPosition = 0;
+let trackDur = 30;
+let trackState = "pause";
+let messageHandler = (type, msg)=>{
+  print("\n","type:"+type, "t:"+msg.t, "src:"+msg.src, "mode:"+msg.state, "pos:"+msg.position, "dur:"+msg.dur);
+  if (type==='music' && msg.src=="musicstate") {
+    trackState = msg.state;
+    trackPosition = msg.position + (trackState==="play"?1:0); // +1 to account for latency.
+    trackDur = msg.dur;
+    if (progressBar) {
+        progressBar.f.stopAutoUpdate();
+        progressBar.f.remove();
+        initProgressBar();
+      }
+    blink() // Indicate when a message arrives.
+  }
+}
+Bangle.on('message', messageHandler);
+
 // cbVolumeSlider is used with volumeSlider
 let cbVolumeSlider = (mode,fb)=>{
   if (mode =="map") Bangle.musicControl({cmd:"vs",extra:Math.round(100*fb/30)}); // vs = Volume Set level
@@ -64,33 +91,6 @@ let init = ()=> {
   draw();
   initProgressBar();
 }
-
-// Get audio levels from Android device
-let audioLevels = {u:30, c:15}; // Init with values to avoid "Uncaught Error: Cannot read property 'u' of undefined" if values were not gathered from Gadgetbridge.
-let audioHandler = (e)=>{audioLevels = e;print(audioLevels);};
-Bangle.on('audio', audioHandler);
-Bangle.musicControl("vg"); // vg = Volume Get level
-
-// Handle music messages
-// Bangle.emit("message", type, msg);
-let trackPosition = 0;
-let trackDur = 30;
-let trackState = "pause";
-let messageHandler = (type, msg)=>{
-  print("\n","type:"+type, "t:"+msg.t, "src:"+msg.src, "mode:"+msg.state, "pos:"+msg.position, "dur:"+msg.dur);
-  if (type==='music' && msg.src=="musicstate") {
-    trackState = msg.state;
-    trackPosition = msg.position + (trackState==="play"?1:0); // +1 to account for latency.
-    trackDur = msg.dur;
-    if (progressBar) {
-        progressBar.f.stopAutoUpdate();
-        progressBar.f.remove();
-        initProgressBar();
-      }
-    blink() // Indicate when a message arrives.
-  }
-}
-Bangle.on('message', messageHandler);
 
 let ebLast = 0; // Used for fix/Hack needed because there is a timeout before the slider is called upon.
 Bangle.on('drag', (e)=>{
